abe1dc7b99266ae12b272b942620767ba55a314d
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-09-21 16:16:12 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {
+  'Slowdive': 'Alison',
+  'My Bloody Valentine': 'Sometimes',
+  
+}
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist['artistName'] = 'songTitle'
+  return playlist
+}
